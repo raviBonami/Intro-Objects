128940cbf871ef38ef9a0a4a25e9050b1edcc00a
Ravi Payasi <ravi.payasi@bonamisoftware.com>
2022-08-24 16:43:59 +0530

    for in loop

--- objectProps.js
+++ objectProps.js
@@ -80,3 +80,105 @@ console.log(person6.fname)
 person6.fname = "pqr";
 console.log(person6.fname) // Still it's value is "abc" as it is not writable
 
+// 2. Accessor Properties - 
+// Similar to data properties, accessor properties also have [[Configurable]] and [[Enumerable]] attributes.
+// But the accessor properties have the [[Get]] and [[Set]] attributes instead of [[Value]] and [[Writable]].
+
+// When you read data from an accessor property, the [[Get]] function is called automatically to return a value. 
+// The default return value of the [[Get]] function is undefined.
+
+// If you assign a value to an accessor property, the [[Set]] function is called automatically.
+
+// To define an accessor property, you must use the Object.defineProperty() method
+
+let person7 = {};
+person7.fname = "harry"
+person7.lname = "potter"
+
+Object.defineProperty(person7, 'fullname',{
+    get: function(){
+        return this.fname + " " +this.lname;
+    },
+    set: function(fname){
+        this.fname = fname;
+        // this.lname = lname;
+    }
+
+})
+
+console.log(person7.fullname)   // Getter
+
+person7.fullname = 'alex';      // Setter
+console.log(person7.fullname)
+
+
+// Object.defineProperties => To define multiple properties at the same time
+let person8 = {}
+Object.defineProperties(person8, {
+    'name' : {
+        value: 'peter'
+    },
+    'age' : {
+        value: 34
+    },
+    'city' : {
+        value: 'new york'
+    }
+})
+// name, age and city properties defined at one time
+
+// Object - get descriptor object
+
+let person9 = {
+    name: 'mike',
+    nation: 'british'
+}
+
+let res = Object.getOwnPropertyDescriptor(person9,'name');
+console.log(res);
+// This will print the descriptor of property 'name' of person9 object like below - 
+// { value: 'mike',
+//   writable: true,
+//   enumerable: true,
+//   configurable: true }
+
+/////////////////////////////////////////////////////////////////////////
+
+// for - in loops
+// Used to enumerate over an object, can enumerate only if its enumeration property is set to true
+// Normally created objects have it set to true only
+
+let animal1 = {
+    name: 'percy',
+    age: 34,
+    city: 'New York'
+}
+
+for(let prop in animal1){
+    console.log(prop + " -> " + animal1[prop])
+}
+
+// When you loop over the properties of an object that inherits from another object, 
+// the for...in statement goes up in the prototype chain and enumerates over inherited properties
+
+let obj1 = {
+    city: 'Jersey'
+}
+
+let obj2 = Object.create(obj1);
+obj2.country = "US"
+for(let prop in obj2){
+    console.log(prop + " -> " + obj2[prop])
+}
+// In above example, it will print both the properties of obj2 as well as the properties of obj1 as 
+// obj2 is inherited from obj1 only
+// If we want to restrict it to its own properties only, we have to use hasOwnProperty
+console.log('-------------------------------------------------------------------')
+
+for(let prop in obj2){
+    if(obj2.hasOwnProperty(prop)){
+
+        console.log(prop + " -> " + obj2[prop]) // will print country only as it is now taking onlt those properties which
+        // are its own
+    }
+}
